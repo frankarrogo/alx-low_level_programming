c74d992e2511eeabab1cd8e496f21ae5967aee3c
Frank Arrogo <frankarrogo@github.com>
2022-04-19 20:07:12 +0000

    Task three

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
-  * print_numbers - Print the numbers 0 to 9
+  * print_numbers - Print the numbers since 0 up to 9
   *
-  * Return: Always 0 (Success)
+  * Return: The numbers since 0 up to 9
   */
 void print_numbers(void)
 {
-	int i;
+	int x;
 
-	for (i = 0; i < 10; i++)
+	for (x = 0; x <= 9; x++)
 	{
-		_putchar(i + '0');
+		_putchar(x + '0');
 	}
 
 	_putchar('\n');
 }
+
