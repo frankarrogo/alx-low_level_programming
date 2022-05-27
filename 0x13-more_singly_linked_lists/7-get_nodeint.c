0ae7fa3ea9412456d9082bee0e1d871a71b36f1f
Frank Arrogo <frankarrogo@github.com>
2022-05-27 17:17:48 +0000

    Task seven

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,26 +1,32 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
-  * sum_listint - ...
-  * @head: ...
-  *
-  * Return: ...
-  */
-int sum_listint(listint_t *head)
+*get_nodeint_at_index - return the nth node of a linked list
+*@head: pointer to the head of the list
+*@index: index of the node required
+*
+*Return: the address of the node
+*/
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	int amount = 0;
+listint_t *cursor = NULL;
+unsigned int i = 0;
 
-	if (head)
-	{
-		while (head)
-		{
-			amount += head->n;
-			head = head->next;
-		}
-	}
-
-	return (amount);
+while (head != NULL)
+{
+if (i <= index)
+{
+if (i == index)
+{
+cursor =  head;
+break;
+}
+head = head->next;
+i++;
+}
+else
+return (NULL);
+}
+return (cursor);
 }
