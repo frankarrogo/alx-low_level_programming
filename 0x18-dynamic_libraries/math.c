ae98a5055d3d5717e438c52cf0cfa52324bb9a82
Frank Arrogo <frankarrogo@github.com>
2022-07-13 13:49:52 +0000

    Task two

---
+++ 0x18-dynamic_libraries/math.c
@@ -0,0 +1,31 @@
+#include "math.h"
+/**
+*add - add 2 nums
+*@a: num1
+*@b: num2
+*Return: sum
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+*sub - sub 2 nums
+*@a: num1
+*@b: num2
+*Return: difference
+*/
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+*mul - mul 2 nums
+*@a: num1
+*@b: num2
+*Return: product
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x18-dynamic_libraries/mathematics.c
@@ -0,0 +1,20 @@
+/**
+*div - div 2 nums
+*@a: num1
+*@b: num2
+*Return: quotient
+*/
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+*mod - mod 2 nums
+*@a: num1
+*@b: num2
+*Return: modulus
+*/
+int mod(int a, int b)
+{
+	return (a % b);
+}
